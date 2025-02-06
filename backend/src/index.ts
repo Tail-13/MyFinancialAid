7d1023be39471ec18a8a5fed7605197d67ee5bb5
Nekotama <tata.rais@bni.co.id>
2025-02-06 17:25:26 +0700

    <new> init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import logger from './utils/logger'
+
+const { APP_PORT } = process.env
+
+dotenv.config() 
+const app = express()
+
+app.use(express.json())
+
+
+app.listen(Number(APP_PORT), () => {
+    logger.info(`server running on port ${Number(APP_PORT)}`)
+})
